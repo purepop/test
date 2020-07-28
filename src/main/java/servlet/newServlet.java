bb7248ae14ba3c317938e72fad2c30f1bb3a23c5
purepop <yishilx@foxmail.com>
2020-07-28 19:50:58 +0800

    newServlet

---
+++ src/main/java/servlet/newServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/newServlet")
+public class newServlet extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException, ServletException {
+        response.getWriter().write("first maven project");
+    }
+    public void doPost(final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException, ServletException {
+        doGet(request, response);
+    }
+}
